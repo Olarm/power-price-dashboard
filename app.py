63f676d8437795ba014b3eec46ac997a3b6dd19b
Ola Rasmus moen <ola.moen@moenmarin.no>
2022-04-03 09:23:31 +0200

    first

---
+++ app.py
@@ -0,0 +1,71 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from flask import Flask
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+from main import get_day_ahead
+
+app = Dash(__name__,)# external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+#server = Flask(__name__)
+#app = Dash(server)
+
+
+colors = {
+    'background': '#311111',
+    'text': '#7FDBFF'
+}
+
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+#df = pd.DataFrame({
+#    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#    "Amount": [4, 1, 2, 2, 4, 5],
+#    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+#})
+
+df = get_day_ahead()
+
+fig = px.line(df, x="time", y="price", line_shape='hv')
+fig.add_vline(x=pd.Timestamp.now())
+
+#fig.update_layout(
+#    plot_bgcolor=colors['background'],
+#    paper_bgcolor=colors['background'],
+#    font_color=colors['text']
+#)
+
+#app.layout = html.Div(children=[
+#    html.H1(children='Hello Dash'),
+#
+#    html.Div(children='''
+#        Dash: A web application framework for your data.
+#    '''),
+#
+#    dcc.Graph(
+#        id='example-graph',
+#        figure=fig
+#    )
+#])
+
+app.layout = html.Div(style={}, children=[
+
+
+    html.Div(children='Day-ahead power price.', style={
+        'textAlign': 'center'
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
